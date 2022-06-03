10cecb1e20fd82e98c32e709318ebcca5d23ae9d
Shedrack Sunday <50990925+Shedrack-Sunday@users.noreply.github.com>
2022-06-04 00:10:48 +0100

    program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s;
+
+s = 122;
+
+while (s >= 97)
+{
+putchar(s);
+s--;
+}
+putchar('\n');
+return (0);
+}
