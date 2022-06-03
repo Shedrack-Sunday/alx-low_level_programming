9239eb6de77b9bc159219711d97300f45ce0aefd
Shedrack Sunday <50990925+Shedrack-Sunday@users.noreply.github.com>
2022-06-03 22:29:20 +0100

    update on file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,18 +11,15 @@ int main(void)
 
 	while (c <= 'z')
 	{
-		putchar(c);
-		c++;
+	putchar(c);
+	c++;
 	}
-
 	c = 'A';
-
 	while (c <= 'Z')
 	{
-		putchar(c);
-		c++;
+	putchar(c);
+	c++;
 	}
-
 	putchar('\n');
 	return (0);
 }
