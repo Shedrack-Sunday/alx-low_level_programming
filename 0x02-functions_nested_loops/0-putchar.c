da8d8012e2169c306ab2fe651f1a94803dd56e67
Shedrack Sunday <hannania002@gmail.com>
2022-06-07 17:47:56 -0700

    adding main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
-
-/*** _putchar - writes the character c to stdout
- * @c: The character to print
+/**
+ * _putchar - writes the character c to stdout
+ * main - Entry point
  *
  * Return: On success 0(sucess)
  * set appropriately.
  */
-int main(void){
+int main(void)
+{
 
      _putchar('c');
 
