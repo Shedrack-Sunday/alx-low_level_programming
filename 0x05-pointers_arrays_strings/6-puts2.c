1e30e0cba7e50aa619833711e72f137cb4552d6a
Shedrack Sunday <Hannania002@gmail.com>
2022-06-14 11:47:01 +0100

    printing strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - prints multiples of two.
+ * @str: first parameter
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, len, start;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		len = i;
+	}
+
+	for (start = 0; start <= len; start++)
+	{
+		if (start % 2 == 0)
+			_putchar(str[start]);
+		else
+			continue;
+	}
+
+	_putchar('\n');
+}
