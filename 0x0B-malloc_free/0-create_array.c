07b4376de8424b0173370c2365b827f48d6a5ba8
Shedrack Sunday <Hannania002@gmail.com>
2022-06-27 16:55:12 +0100

    Created main.h and array function file in malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include  "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - Create an array, and initialiazes it with a specific
+ * char @c
+ *
+ * @size: Size of the array of chars to be created
+ *
+ * @c: Char value to initialize the array with
+ *
+ * Return: Pointer to the array created OR NULL if @size = 0 OR
+ * the creation of the array fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int j = 0;
+
+	p = malloc(size);
+	if (size == 0 || p == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < size; j++)
+	{
+	p[j] = c;
+	}
+	if (!p)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (p);
+	}
+}
