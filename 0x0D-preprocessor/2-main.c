e8ed055cb01056e8886f85ec311a2daf75b46acf
Shedrack Sunday <Hannania002@gmail.com>
2022-07-04 14:52:39 +0100

    Editing Directory files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main-print the name of the source file.
+ *Return: 0-success, non-zero-fail
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
