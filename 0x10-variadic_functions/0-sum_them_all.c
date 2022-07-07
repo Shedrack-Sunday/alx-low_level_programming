8bd8b68e9ed1e0d6ea82ec7a62969ae6a31cd75f
Shedrack Sunday <Hannania002@gmail.com>
2022-07-07 16:50:37 +0100

    changes to comments on file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,7 +4,7 @@
 /**
  * sum_them_all - Returns the sum of all its paramters.
  * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * @...: A variable no. of paramters to calculate the sum of.
  *
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
